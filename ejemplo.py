8e051a5da986235de3ac4333d6e006a1a2cbbdd5
joseAriass <josedarcab@gmail.com>
2023-05-22 13:27:16 -0500

    Se agrega el Ejemplo

---
+++ ejemplo.py
@@ -0,0 +1,9 @@
+# Pedir al usuario que ingrese dos números
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+# Calcular la suma de los dos números
+suma = num1 + num2
+
+# Mostrar el resultado
+print("La suma de", num1, "y", num2, "es:", suma)
